62f14f00d7c11797e9a1c2e21833a4c6c9014a59
TooPositive <trustyourkarma@gmail.com>
2020-10-26 22:41:02 +0100

    AddSignalR

--- Api/Functions/BookShopFunction.cs
+++ Api/Functions/BookShopFunction.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -23,6 +24,15 @@ namespace BlazorApp.Api
         {
             _booksDBContext = booksDBContext;
         }
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo GetSignalRInfo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "booksHub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
 
         [FunctionName("AllBooks")]
         public async Task<IActionResult> GetBooks(
@@ -46,6 +56,7 @@ namespace BlazorApp.Api
         [FunctionName("RefreshBooks")]
         public async Task<IActionResult> PostRefreshBooks(
                 [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+                [SignalR(HubName = "booksHub")] IAsyncCollector<SignalRMessage> signalRMessages,
                 ILogger log)
         {
             try
@@ -53,6 +64,15 @@ namespace BlazorApp.Api
                 var books = await _booksDBContext.Books.ToListAsync();
                 books.ForEach(x => x.IsBought = false);
                 await _booksDBContext.SaveChangesAsync();
+
+
+                await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "booksRefreshed",
+                    Arguments = books.ToArray()
+                });
+
                 return new OkObjectResult("All books are mark as not bought.");
             }
             catch (Exception)
