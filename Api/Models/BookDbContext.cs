97db21e5e6b1b3b1b99aec83bd7046be4afe7b78
unknown <trustyourkarma@gmail.com>
2020-10-26 20:35:38 +0100

    book endpoints

---
+++ Api/Models/BookDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Api.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Shared/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Shared.Models
+{
+    public class Book
+    {
+        public string Author  { get; set; }
+        public string Title  { get; set; }
+        public double Price  { get; set; }
+        public bool IsBought { get; set; }
+    }
+}
